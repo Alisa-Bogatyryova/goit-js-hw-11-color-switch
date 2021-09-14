f34a8cb33493122a436f4060ded7ad97c76c9904
Alisa-Bogatyryova <alisabogatyryova@gmail.com>
2021-09-14 10:13:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,38 @@ const colors = [
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+const refs = {
+  btnStart: document.querySelector('button[data-action="start"]'),
+  btnStop: document.querySelector('button[data-action="stop"]'),
+};
+
+const switchColors = {
+  intervalId: null,
+
+  start() {
+    refs.btnStart.disabled = true;
+    refs.btnStop.disabled = false;
+
+    this.intervalId = setInterval(() => {
+      const currentIndex = randomIntegerFromInterval(0, colors.length - 1);
+      colors.map((el, ind) => {
+        if (currentIndex === ind) {
+          document.body.style.backgroundColor = el;
+          // console.log(el);
+        }
+      });
+    }, 1000);
+  },
+
+  stop() {
+    refs.btnStop.disabled = true;
+    refs.btnStart.disabled = false;
+
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  },
+};
+
+refs.btnStart.addEventListener('click', switchColors.start.bind(switchColors));
+refs.btnStop.addEventListener('click', switchColors.stop.bind(switchColors));
